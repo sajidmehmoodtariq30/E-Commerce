183fd9ecfdecd37b87796e992e66524166a3268a
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2024-08-19 20:53:23 +0500

    added policy section

---
+++ src/JS/Components/policySection.js
@@ -0,0 +1,46 @@
+import "../../Styles/components/policy-section.css";
+
+const policySection =`
+<div class="container grid grid-four--cols">
+  <div class="div-policy">
+    <div class="icons">
+      <i class="fa-solid fa-truck-fast"></i>
+    </div>
+    <div class="div-policy-text">
+      <p>worldwide shipping</p>
+      <p>order above $100</p>
+    </div>
+  </div>
+
+  <div class="div-policy">
+    <div class="icons">
+      <i class="fa-solid fa-rotate"></i>
+    </div>
+    <div class="div-policy-text">
+      <p>Easy 30 Day Returns</p>
+      <p>Back Returns in 7 Days</p>
+    </div>
+  </div>
+
+  <div class="div-policy">
+    <div class="icons">
+      <i class="fa-solid fa-hand-holding-dollar"></i>
+    </div>
+    <div class="div-policy-text">
+      <p>money back guarantee</p>
+      <p>guarantee with in 30-Days</p>
+    </div>
+  </div>
+
+  <div class="div-policy">
+    <div class="icons">
+      <i class="fa-solid fa-headset"></i>
+    </div>
+    <div class="div-policy-text">
+      <p>Easy Online Support</p>
+      <p>24/7 Any time support</p>
+    </div>
+  </div>
+</div>
+`
+export default policySection
--- src/JS/pages/mainPage.js
+++ src/JS/pages/mainPage.js
@@ -4,6 +4,7 @@ import topHeader from '../Components/topHeader.js';
 import bottomHeader from '../Components/bottomHeader.js';
 import mainSection from '../Components/heroSection.js';
 import extraProductSection from '../Components/extraProductSection.js';
+import policySection from '../Components/policySection.js'
 
 const renderMainPage = () => {
     const navbar = document.getElementById('section-navbar');
@@ -24,6 +25,12 @@ const renderMainPage = () => {
         extraProduct.innerHTML = extraProductSection;
     } else {
         console.error("Extra Product element element not found");
+    }
+    const policy = document.getElementById('section-policy');
+    if (policy) {
+        policy.innerHTML = policySection;
+    } else {
+        console.error("policy element element not found");
     }
 };
 
